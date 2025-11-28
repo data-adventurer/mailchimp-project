5a514e637c2bcc0b82469feb9ee609f1f98320be
Lorraine Ferrusi <lferrusi94@gmail.com>
2025-11-28 14:10:12 +0000

    Before presentation

--- models/facts/mailchimp__fct_campaigns.sql
+++ models/facts/mailchimp__fct_campaigns.sql
@@ -3,19 +3,43 @@ with stg_mailchimp__campaigns as (
     select * from {{ ref('stg_mailchimp__campaigns') }}
   
 ),
-mailchimp__fct_campaigns as (
-    
+
+int_mailchimp__campaign_report_summary as (
+
+    select * from {{ ref('int_mailchimp__campaign_report_summary') }}
+  
+),
+
+total_emails_sent as (
     select
-
-        id as campaign_id,
-        report_summary:opens::int as opens,
-        report_summary:unique_opens::int as unique_opens,
-        report_summary:open_rate::float as open_rate,
-        report_summary:clicks::int as clicks,
-        report_summary:subscriber_clicks::int as subscriber_clicks,
-        report_summary:click_rate::float as click_rate
-
+        id
+        ,status
+        ,SUM(emails_sent) as emails_sent
     from stg_mailchimp__campaigns
+    group by 1,2
+),
+
+join_tables_together as (
+    select 
+        campaign_id
+        ,opens
+        ,unique_opens
+        ,open_rate
+        ,clicks
+        ,subscriber_clicks
+        ,click_rate
+        ,emails_sent
+        ,status
+    from int_mailchimp__campaign_report_summary as crs 
+    left join total_emails_sent as tms
+    on crs.campaign_id = tms.id
+),
+
+mailchimp__fct_campaigns as (
+    select 
+        *
+    from join_tables_together
+    where status = 'sent'
 )
 
-select * from int_mailchimp__fct_campaigns
+select * from mailchimp__fct_campaigns
